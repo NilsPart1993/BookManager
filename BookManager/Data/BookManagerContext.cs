2b98eda6766ba9c7173b4aa92ee9b4828b6b7eab
Nils Part <Nils.Part@protonmail.com>
2023-10-18 00:52:32 +0200

    Added, Book model, Scaffolded CRUD for Model OP, initial migration, Update-Database

---
+++ BookManager/Data/BookManagerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookManager.Models;
+
+namespace BookManager.Data
+{
+    public class BookManagerContext : DbContext
+    {
+        public BookManagerContext (DbContextOptions<BookManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookManager.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ BookManager/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BookManager.Data;
+using BookManager.Models;
+
+namespace BookManager.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BookManager.Data.BookManagerContext _context;
+
+        public IndexModel(BookManager.Data.BookManagerContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Book != null)
+            {
+                Book = await _context.Book.ToListAsync();
+            }
+        }
+    }
+}
